0a022abf3280020707a69f8fa440ec85fcff5f6b
Atila Lima <atilalimade@gmail.com>
2026-07-20 11:54:28 -0300

    fix: corrigindo importação circular

--- src/command_mapper/normalizers/__init__.py
+++ src/command_mapper/normalizers/__init__.py
@@ -2,10 +2,8 @@
 
 from src.command_mapper.normalizers.english import EnglishSpeechNormalizer
 from src.command_mapper.normalizers.portuguese import PortugueseSpeechNormalizer
-from src.command_mapper.normalizer_dispatchers import resolve_command_normalizer
 
 __all__ = [
     "EnglishSpeechNormalizer",
     "PortugueseSpeechNormalizer",
-    "resolve_command_normalizer",
 ]
